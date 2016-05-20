a6eb3f3e0063dfca7eaf1def696b9ff7c3c7626f
y917059177y <x7985959683y>
2016-05-21 00:59:25 +0800

    增加Fetchr类来获取网上的数据

--- app/src/main/java/com/example/xyl/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/example/xyl/photogallery/PhotoGalleryFragment.java
@@ -1,13 +1,17 @@
 package com.example.xyl.photogallery;
 
 import android.app.Fragment;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.io.IOException;
 
 /**
  * Created by XyL on 2016/5/21.
@@ -15,10 +19,19 @@ import android.view.ViewGroup;
  */
 public class PhotoGalleryFragment extends Fragment {
 
+    private static final String TAG = "PhotoGalleryFragment";
+
     private RecyclerView mPhotoRecyclerView;
 
     public static Fragment newInstance() {
         return new PhotoGalleryFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+        new FetchItemsTask().execute();
     }
 
     @Nullable
@@ -31,5 +44,20 @@ public class PhotoGalleryFragment extends Fragment {
         mPhotoRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
 
         return view;
+    }
+
+    private class FetchItemsTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                String result = new TumblrFetchr().getUrlString("https://www.bignerdranch.com");
+
+                Log.i(TAG, "Fetched contents of URL: " + result);
+            } catch (IOException e) {
+                Log.e(TAG, "Failed to fetch URL: ", e);
+            }
+            return null;
+        }
     }
 }
